c4d60757b8da34afeb6730279d758d4ee6ef75dd
ElazAzrt <elaz263@gmail.com>
2026-08-02 17:17:37 +0500

    chore: remove local smoke test scripts

--- api/smoke-srv.js
+++
@@ -1,4 +0,0 @@
-const { default: handler } = require('./.smoke/index.js');
-const http = require('http');
-http.createServer((req, res) => handler(req, res).catch(e => { res.statusCode = 500; res.end(String(e)); }))
-  .listen(3001, () => console.log('UP'));
